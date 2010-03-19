32293cc6ecf920c4f69cd902e9bb278948597252
Bellière Ludovic <xrogaan@gmail.com>
2010-03-19 08:36:56 +0100

    number isn't useless. I swear \!

--- contrib/multiname-generator.php
+++ contrib/multiname-generator.php
@@ -9,7 +9,7 @@ require('mudnames.php');
 
 $gname = new mudnames();
 
-$names = $gname->generates_several_names(5,'random');
+$names = $gname->generates_several_names($number,'random');
 echo implode('<br />', $names);
 
 echo "<br/><br/>Memory peak ~" , round(memory_get_peak_usage()/1024, 2) , "ko";
