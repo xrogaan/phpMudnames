01f4b9629869e44f8111081107bc034b01f4042f
Ludovic Bellière <xrogaan@gmail.com>
2010-07-24 13:48:34 +0200

    Typo

--- mudnames.php
+++ mudnames.php
@@ -15,13 +15,13 @@ class Mudnames_Dictionnaries {
     protected $_dictionnaries = array();
     protected $_dictionnaries_instance = array();
 
-	/**
-	 * Capability : different possibilities of connection between several particles
-	 */
-	private $_caplist = array(
-		"PS", "PMS", "PM", "N", "X",
-		"NA", "XA" , "NX"
-	);
+    /**
+     * Capability : different possibilities of connection between several particles
+     */
+    private $_caplist = array(
+        "PS", "PMS", "PM", "N", "X",
+        "NA", "XA" , "NX"
+    );
 
     public $_actions = array();
 
@@ -44,8 +44,8 @@ class Mudnames_Dictionnaries {
         $dh = opendir($this->_directory);
         while(false !== ($file = readdir($dh))) {
             if ($file[0] == '.' || substr($file,-4) == '.cap') {
-				continue;
-			}
+                continue;
+            }
             $this->_dictionnaries[$file] = $this->_directory . $file;
         }
         closedir($dh);
@@ -90,28 +90,28 @@ class Mudnames_Dictionnaries {
     public function get_name($dictionnary) {
         $current_cap = $this->_dictionnaries_instance[$dictionnary]->select_capability();
 
-		switch ($current_cap) {
-			case 'N':
-				$capability = 'NN';
-				break;
-			case 'X':
-				$capability = 'XX';
-				break;
-			case 'XA':
-				$capability = 'N' . (rand(0,1) ? 'A' : 'X' );
-				break;
-			case 'NX':
-				$capability = (rand(0, 1) ? 'N' : 'A') . 'A';
-				break;
-			default:
-				$capability = $current_cap;
-		}
+        switch ($current_cap) {
+            case 'N':
+                $capability = 'NN';
+                break;
+            case 'X':
+                $capability = 'XX';
+                break;
+            case 'XA':
+                $capability = 'N' . (rand(0,1) ? 'A' : 'X' );
+                break;
+            case 'NX':
+                $capability = (rand(0, 1) ? 'N' : 'A') . 'A';
+                break;
+            default:
+                $capability = $current_cap;
+        }
 
-		$name = '';
-		for ($i = 0, $len = strlen($capability); $i < $len; $i++) {
-			$particle = $this->_dictionnaries_instance[$dictionnary]->plite2full($capability[$i]);
-			$name.= $this->_dictionnaries_instance[$dictionnary]->random_particle_from($particle);
-		}
+        $name = '';
+        for ($i = 0, $len = strlen($capability); $i < $len; $i++) {
+            $particle = $this->_dictionnaries_instance[$dictionnary]->plite2full($capability[$i]);
+            $name.= $this->_dictionnaries_instance[$dictionnary]->random_particle_from($particle);
+        }
 
         $name = ucfirst($name);
         $this->_actions['dictionnaries'][$dictionnary]['generated'][$name] = array (
@@ -119,19 +119,19 @@ class Mudnames_Dictionnaries {
             'particles' => $particle
         );
 
-		return $name;
+        return $name;
     }
 }
 
 class Mudnames_Dictionnary {
     /**
-	 * Name's Particles
-	 * Currently used : PRE, MID, SUF.
-	 */
-	private $_particle = array(
-		'PRE' => 'P',  'MID' => 'M',  'SUF'  => 'S',
-		'NOUN' => 'N', 'ADJ' => 'A',  'NADJ' => 'X',
-	);
+     * Name's Particles
+     * Currently used : PRE, MID, SUF.
+     */
+    private $_particle = array(
+        'PRE' => 'P',  'MID' => 'M',  'SUF'  => 'S',
+        'NOUN' => 'N', 'ADJ' => 'A',  'NADJ' => 'X',
+    );
 
     protected $capabilities = array();
     protected $dictionnary_name;
@@ -225,177 +225,177 @@ class Mudnames_Dictionnary {
     }
 
     /**
-	 * Détecte si un fichier doit avoir une génération statique de noms.
-	 */
-	function force_capability($cap) {
-		if (in_array($cap,$this->capabilities)) {
-			$this->currentcap = $cap;
-			$this->forcedcap = true;
-			return true;
-		}
-		return false;
-	}
-	
-	/**
-	 * Sélectionne une association de capacité de façons aléatoire et la retourne.
-	 */
-	public function select_capability() {
+     * Détecte si un fichier doit avoir une génération statique de noms.
+     */
+    function force_capability($cap) {
+        if (in_array($cap,$this->capabilities)) {
+            $this->currentcap = $cap;
+            $this->forcedcap = true;
+            return true;
+        }
+        return false;
+    }
+    
+    /**
+     * Sélectionne une association de capacité de façons aléatoire et la retourne.
+     */
+    public function select_capability() {
 
-		self::check_cap_file();
+        self::check_cap_file();
 
-		// If a forced capability exists, we apply it.
-		if (!empty($this->forced_capability)) {
-			foreach ($this->forced_capability as $fnct => $args) {
-				if ($fnct == 'void') {
-					self::force_capability($args);
-				} else {
-					call_user_func_array(array($this,$fnct),$args);
-				}
-			}
-		}
+        // If a forced capability exists, we apply it.
+        if (!empty($this->forced_capability)) {
+            foreach ($this->forced_capability as $fnct => $args) {
+                if ($fnct == 'void') {
+                    self::force_capability($args);
+                } else {
+                    call_user_func_array(array($this,$fnct),$args);
+                }
+            }
+        }
 
-		if (!$this->forcedcap) {
-			if (count($this->capabilities)>1) {
-				$this->currentcap = $this->capabilities[rand(0, count($this->capabilities) - 1)];
-			} elseif (count($this->capabilities) == 1) {
-				$this->currentcap = $this->capabilities[0];
-			} else {
-				trigger_error ($this->directory.$this->file . ":select_capability: No capability found.",E_USER_WARNING);
-			}
-		}
+        if (!$this->forcedcap) {
+            if (count($this->capabilities)>1) {
+                $this->currentcap = $this->capabilities[rand(0, count($this->capabilities) - 1)];
+            } elseif (count($this->capabilities) == 1) {
+                $this->currentcap = $this->capabilities[0];
+            } else {
+                trigger_error ($this->directory.$this->file . ":select_capability: No capability found.",E_USER_WARNING);
+            }
+        }
 
-		return $this->currentcap;
-	}
+        return $this->currentcap;
+    }
 
-	/**
-	 * Check if a capability file is present for the current file.
-	 *
+    /**
+     * Check if a capability file is present for the current file.
+     *
      * If it's present, its content is loaded into the core. It is usefull
      * to force actions in the choice of particles. Some dictionnary have 3
      * set of particles like 'PMS', but if the object return only a name
      * containing the parts 'P' and 'S', the name could mean absolutly nothing.
      * So, the capability file is here to force the object to get the full
      * capacities of the dictionnary by triggering the method self::force_capability().
-	 *
-	 * .cap files format :
-	 * [functionName][:arguments]
-	 */
-	private function check_cap_file() {
-		// Non-lethal error here. If we can't open it, we leave it alone.
-		if (file_exists($this->filename. '.cap') && is_readable($this->filename . '.cap')) {
+     *
+     * .cap files format :
+     * [functionName][:arguments]
+     */
+    private function check_cap_file() {
+        // Non-lethal error here. If we can't open it, we leave it alone.
+        if (file_exists($this->filename. '.cap') && is_readable($this->filename . '.cap')) {
 
-			$this->forced_cap = true;
-			$handle = @fopen($this->filename . '.cap', 'r');
+            $this->forced_cap = true;
+            $handle = @fopen($this->filename . '.cap', 'r');
 
-			if ($handle) {
-				while (!feof($handle)) {
-					$buffer = fgets($handle, 4096);
+            if ($handle) {
+                while (!feof($handle)) {
+                    $buffer = fgets($handle, 4096);
 
-					if (!empty($buffer)) {
-						if (ctype_lower($buffer[0])) {
-							list($function, $args) = explode(':',$buffer);
-						} else {
-							$function = 'void';
-							$args = $buffer;
-						}
+                    if (!empty($buffer)) {
+                        if (ctype_lower($buffer[0])) {
+                            list($function, $args) = explode(':',$buffer);
+                        } else {
+                            $function = 'void';
+                            $args = $buffer;
+                        }
 
-						if ($function != 'void' && !method_exists($this,$function)) {
-							trigger_error('Method `<em>'.$function.'</em>` does\'nt exists',E_USER_WARNING);
-						} else {
-							$this->forced_capability[$function] = explode(',',trim($args));
-						}
-					}
-				} // -- end while
-				fclose($handle);
-			} // -- end if
-			return true;
-		} else {
-			return false;
-		}
-	}
+                        if ($function != 'void' && !method_exists($this,$function)) {
+                            trigger_error('Method `<em>'.$function.'</em>` does\'nt exists',E_USER_WARNING);
+                        } else {
+                            $this->forced_capability[$function] = explode(',',trim($args));
+                        }
+                    }
+                } // -- end while
+                fclose($handle);
+            } // -- end if
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	/**
-	 * Sors une partie de nom aléatoire selon le tag et le fichier associé.
-	 */
-	public function random_particle_from($particle) {
-		if (!isset($this->file_particles[$particle]))
-			trigger_error ($this->filename . ":load_random_particle: Unknow particle '$particle'.",E_USER_WARNING);
+    /**
+     * Sors une partie de nom aléatoire selon le tag et le fichier associé.
+     */
+    public function random_particle_from($particle) {
+        if (!isset($this->file_particles[$particle]))
+            trigger_error ($this->filename . ":load_random_particle: Unknow particle '$particle'.",E_USER_WARNING);
 
-		if (empty($this->file_particles[$particle]))
-			return '';
+        if (empty($this->file_particles[$particle]))
+            return '';
 
-		do {
-			$randomId = rand(0,count($this->file_particles[$particle])-1);
-			if (isset($this->file_particles[$particle][$randomId]))
-				break;
-		} while(1);
+        do {
+            $randomId = rand(0,count($this->file_particles[$particle])-1);
+            if (isset($this->file_particles[$particle][$randomId]))
+                break;
+        } while(1);
 
-		$this->debug['particles_used'][$particle] = $this->file_particles[$particle][$randomId];
+        $this->debug['particles_used'][$particle] = $this->file_particles[$particle][$randomId];
 
-		return $this->file_particles[$particle][$randomId];
+        return $this->file_particles[$particle][$randomId];
 
-	}
+    }
 
-	/**
-	 * retourne la liste des parties présente dans le fichier dictionnaire
-	 *
-	 * @return array
-	 */
-	public function get_file_particle_list() {
-		return array_keys($this->file_particles);
-	}
+    /**
+     * retourne la liste des parties présente dans le fichier dictionnaire
+     *
+     * @return array
+     */
+    public function get_file_particle_list() {
+        return array_keys($this->file_particles);
+    }
 
-	public function get_file_capability_list() {
-		return $this->capabilities;
-	}
+    public function get_file_capability_list() {
+        return $this->capabilities;
+    }
 
-	public function __toString() {
-		return $this->filename;
-	}
+    public function __toString() {
+        return $this->filename;
+    }
 
-	/**
-	 * Retourne la version courte d'un tag
-	 */
-	public function pfull2lite($par) {
-		return $this->_particle[$par];
-	}
+    /**
+     * Retourne la version courte d'un tag
+     */
+    public function pfull2lite($par) {
+        return $this->_particle[$par];
+    }
 
-	/**
-	 * Retourne la version longue d'un tag
-	 */
-	public function plite2full($par) {
-		return array_search($par,$this->_particle);
-	}
+    /**
+     * Retourne la version longue d'un tag
+     */
+    public function plite2full($par) {
+        return array_search($par,$this->_particle);
+    }
 
-	public function is_forced() {
-		return $this->forcedcap;
-	}
+    public function is_forced() {
+        return $this->forcedcap;
+    }
 
 }
 
 class Mudnames {
     protected static $_instance = null;
 
-	private $directory;
-	private $files = array();
-	private $_dictionnaries;
-	private $particle_used = array();
-	private $capability;
-	
-	private $info = array(
-		'dictionnaries' => '',
-		'particles_used' => array(),
-		'capability' => '',
-		'is_forced' => false,
-	);
-	
-	protected function __construct($config='') {
+    private $directory;
+    private $files = array();
+    private $_dictionnaries;
+    private $particle_used = array();
+    private $capability;
+    
+    private $info = array(
+        'dictionnaries' => '',
+        'particles_used' => array(),
+        'capability' => '',
+        'is_forced' => false,
+    );
+    
+    protected function __construct($config='') {
         if (!empty($config)) {
             $this->_dictionnaries = new Mudnames_Dictionnaries($config);
         } else {
             $this->_dictionnaries = new Mudnames_Dictionnaries();
         }
-	}
+    }
 
     public static function getInstance($config = array(), $auto_create = true)
     {
@@ -404,14 +404,14 @@ class Mudnames {
         }
         return static::$_instance;
     }
-	
-	public static function generate_name_from($file='') {
+    
+    public static function generate_name_from($file='') {
 
         $mudnames = Mudnames::getInstance();
         $dictionnary = $mudnames->_dictionnaries->open_dictionnary($file);
         $mudnames->_dictionnaries->get_name($file);
         
-	}
+    }
 
     public static function generates_several_names($number, $file='') {
         $number = (int) $number;
